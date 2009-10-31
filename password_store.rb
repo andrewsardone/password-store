1d98e46da97b2bfeb8ebb8955d1c1523b675585f
Andrew Sardone <andrew.sardone@gmail.com>
2009-10-31 00:35:03 -0400

    First pass at a simple Account model mixin, with DataMapper setup
    Also, note that all content will be going out as utf-8

--- password_store.rb
+++ password_store.rb
@@ -1,2 +1,39 @@
 require 'rubygems'
 require 'sinatra'
+require 'dm-core'
+require 'dm-timestamps'
+
+configure :development do 
+  DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+# DataMapper doesn't do Single Table Inheritance (STI) like ActiveRecord
+# does. Instead, create a module with class evaluated property declarations
+# and include it within your model classes.
+module Account
+  def self.included(other)
+    other.class_eval <<-EOS
+      property :id,          Serial
+      property :username,    String
+      property :password,    String
+      property :url,         String
+      property :created_at,  DateTime
+      property :updated_at,  DateTime
+    EOS
+  end
+end
+
+class Server
+  include DataMapper::Resource
+  include Account
+  
+  property :type, String
+end
+
+# Create or upgrade all tables at once, like magic
+DataMapper.auto_upgrade!
+
+# set utf-8 for outgoing
+before do
+  headers 'Content-Type' => 'text/html; charset=utf-8'
+end
